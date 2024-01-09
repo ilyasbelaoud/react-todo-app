9ba1cd5ba1306d8968c0221ce770e568b2b84e16
pmmachadov <pmmachadov@gmail.com>
2024-01-09 12:19:28 +0100

    TodoApp React Javascript

--- src/DarkModeToggle.js
+++ src/DarkModeToggle.js
@@ -1,22 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import './DarkModeToggle.scss';
+import './DarkModeToggle.css';
 
 const getUserTheme = () => {
 	const theme = localStorage.getItem('theme') || 'dark';
-	return theme === 'dark' ? true : false;
+	return theme === 'dark';
 };
 
 const DarkModeToggle = () => {
 	const [isDarkMode, setIsDarkMode] = useState(getUserTheme());
-	
+
 	useEffect(() => {
 		document.documentElement.className = `${isDarkMode && 'dark'}`;
 		localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
 	}, [isDarkMode])
-	
+
 	return (
 		<div className='toggle-btn'>
-			<input type="checkbox" id="toggle" checked={isDarkMode} onChange={(e) => setIsDarkMode(e.target.checked)}/>
+			<input type="checkbox" id="toggle" checked={ isDarkMode } onChange={ (e) => setIsDarkMode(e.target.checked) } />
 			<label htmlFor="toggle"></label>
 		</div>
 	)
